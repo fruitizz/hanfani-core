1044d545f97278225d0422d208aeb3e5758fc97e
Elhaj-Mashkour <azizmashkour@gmail.com>
2026-07-15 15:39:16 +0200

    refactor(core): extract handoff module

---
+++ src/handoff/index.ts
@@ -0,0 +1,9 @@
+export {
+  decodeHandoff,
+  encodeHandoff,
+  HandoffPayloadSchema,
+  TicketHandoffPayloadSchema,
+  type HandoffPayload,
+  type TicketHandoffPayload,
+} from './handoff.js'
+export { handoffNote, type HandoffNoteValue } from './handoffNote.js'
